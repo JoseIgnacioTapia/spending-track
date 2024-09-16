c2bd432711c317d490d5b214438ff2cb5fa9cfbf
JoseIgnacioTapia <josignaciot@gmail.com>
2024-09-16 12:24:10 -0300

    Guest component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-function HomePage() {
-  return <h1>HomePage</h1>;
+import Guest from '@/components/Guest';
+import { currentUser } from '@clerk/nextjs/server';
+
+async function HomePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
+  return (
+    <main>
+      <h1>Welcome, {user.firstName}</h1>
+    </main>
+  );
 }
 
 export default HomePage;
