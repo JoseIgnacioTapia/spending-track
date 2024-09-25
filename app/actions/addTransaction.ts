e032083379b1e75b8bc188aa2306fe8b73cfede4
JoseIgnacioTapia <josignaciot@gmail.com>
2024-09-25 16:17:49 -0300

    Save transaction to Neon DB

--- app/actions/addTransaction.ts
+++ app/actions/addTransaction.ts
@@ -1,5 +1,7 @@
 'use server';
 import { auth } from '@clerk/nextjs/server';
+import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
 
 interface TransactionData {
   text: string;
@@ -17,6 +19,13 @@ export async function addTransaction(
   const textValue = formData.get('text');
   const amountValue = formData.get('amount');
 
+  if (!textValue || textValue === '' || !amountValue) {
+    return { error: 'Text or amount is missing' };
+  }
+
+  const text: string = textValue.toString();
+  const amount: number = parseFloat(amountValue.toString());
+
   // Get logged in user
   const { userId } = auth();
 
@@ -25,17 +34,19 @@ export async function addTransaction(
     return { error: 'User not found' };
   }
 
-  if (!textValue || textValue === '' || !amountValue) {
-    return { error: 'Text or amount is missing' };
+  try {
+    const transactionData: TransactionData = await db.transaction.create({
+      data: {
+        text,
+        amount,
+        userId,
+      },
+    });
+
+    revalidatePath('/');
+
+    return { data: transactionData };
+  } catch (error) {
+    return { error: 'Transaction not added' };
   }
-
-  const text: string = textValue.toString();
-  const amount: number = parseFloat(amountValue.toString());
-
-  const transactionData: TransactionData = {
-    text,
-    amount,
-  };
-
-  return { data: transactionData };
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { ClerkProvider } from '@clerk/nextjs';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Roboto } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
@@ -22,6 +24,7 @@ export default function RootLayout({
         <body className={roboto.className}>
           <Header />
           <main>{children}</main>
+          <ToastContainer />
         </body>
       </html>
     </ClerkProvider>
