56e8f2aa8b7e52c955995fa800be843e60a3fba5
JoseIgnacioTapia <josignaciot@gmail.com>
2024-09-30 10:58:52 -0300

    Delete Button

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -5,12 +5,30 @@ import { toast } from 'react-toastify';
 function TransactionItem({ transaction }: { transaction: Transaction }) {
   const sign = transaction.amount < 0 ? '-' : '+';
 
+  const handleDeleteTransaction = async (transactionId: string) => {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete this transaction?'
+    );
+
+    if (!confirmed) return;
+
+    await deleteTransaction(transactionId);
+
+    toast.success('Transaction deleted');
+  };
+
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}
       <span>
         {sign}${addCommas(Math.abs(transaction.amount))}
       </span>
+      <button
+        className='delete-btn'
+        onClick={() => handleDeleteTransaction(transaction.id)}
+      >
+        x
+      </button>
     </li>
   );
 }
